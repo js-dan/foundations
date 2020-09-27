2adf6c25dc18a23855161ab1edcec5039ce6f378
Daniel Silva <djs@cin.ufpe.br>
2020-09-27 13:05:42 -0300

    routes update

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,41 +1,68 @@
+const { request } = require('express');
 const express = require('express');
+const { uuid } = request('uuidv4');
 
 const app = express();
 
 app.use(express.json());
+
+const projects = [];
 
 app.get('/',(request, response) => {
   return response.json({ message: 'Hello You'});
 });
 
 app.get('/projects',(request, response) => {
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-  ]);
+  const { title } = request.query;
+
+  const results = title
+    ? projects.filter(projects => project.title.includes(title))
+    : projects;
+
+  return response.json(results);
 });
 
 app.post('/projects',(request, response) => {
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+  const { title, owner } = request.body;
+
+  const project = { id: uuid(), title, owner };
+  projects.push(project);
+
+  return response.json(project);
 });
 
 app.put('/projects/:id',(request, response) => {
-  return response.json([
-    'Projeto 4',
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+  const { id } = request.params;
+  const { title, owner } = request.body;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found.'});
+  }
+
+  const project = {
+    id,
+    title,
+    owner,
+  };
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
 });
 
 app.delete('/projects/:id',(request, response) => {
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-  ]);
+  const { id } = request.params;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found.'});
+  }
+
+  projects.splice(projectIndex, 1);
+  return response.status(204).send();
 });
 
 app.listen(3333, () => {
